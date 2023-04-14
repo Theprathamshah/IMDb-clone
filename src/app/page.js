d1febe7c6213b55a989bba10cf4174f7d92b92ff
PrathamShah29 <119159666+PrathamShah29@users.noreply.github.com>
2023-04-14 20:21:10 +0530

    Added till fetching data from API

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,25 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Results from "@/components/Results";
+const API_KEY = process.env.API_KEY;
 
-const inter = Inter({ subsets: ["latin"] });
+export default async function Home({ searchParams }) {
+	const genre = searchParams.genre || "fetchTrending";
+	const res = await fetch(
+		`https://api.themoviedb.org/3/${
+			genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+		}?api_key=${API_KEY}&language=en-US&page=1`,
+		{ next: { revalidate: 10000 } }
+	);
 
-export default function Home() {
-	return <main className=" text-3xl  text-emerald-800 "></main>;
+	if (!res.ok) {
+		throw new Error("Failed to fetch data"); //This will throw an error
+	}
+	const data = await res.json();
+
+	const results = data.results;
+
+	return (
+		<div>
+			<Results results={results} />
+		</div>
+	);
 }
