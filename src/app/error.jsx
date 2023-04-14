ab9cccbc90ea73fd2ff3e053ae023d7a3c106320
PrathamShah29 <119159666+PrathamShah29@users.noreply.github.com>
2023-04-14 21:13:23 +0530

    Handle error page

---
+++ src/app/error.jsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import { useEffect } from "react";
+
+const error = ({ error, reset }) => {
+	useEffect(() => {
+		console.log(error);
+	}, [error]);
+	return (
+		<div className="text-center mt-10 ">
+			<h1>Something went wrong</h1>
+			<button
+				className=" hover:bg-amber-600 border-amber-600  "
+				onClick={() => reset()}
+			>
+				Try Again
+			</button>
+		</div>
+	);
+};
+
+export default error;
