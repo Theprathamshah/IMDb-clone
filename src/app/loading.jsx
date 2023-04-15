1d7d33a62f8209c9516e5a6b4746c318470b1a6f
PrathamShah29 <119159666+PrathamShah29@users.noreply.github.com>
2023-04-15 17:40:29 +0530

    Comitted till loader effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+	return (
+		<div className="flex justify-center ">
+			<img className=" h-84 " src="spinner.svg" alt="Loading..." />
+		</div>
+	);
+};
+
+export default loading;
